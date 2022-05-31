9e3cf447f023b4bc375d4b00d70d1c3c38b2e1bf
sgodzheva <svetlozara.godzheva@gmail.com>
2022-05-31 09:00:14 +0300

    Initial setup for Selenium

--- src/Trello.Tests.Selenium/Trello.Tests.Selenium/UnitTest1.cs
+++ src/Trello.Tests.Selenium/Trello.Tests.Selenium/UnitTest1.cs
@@ -1,4 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
 
 namespace Trello.Tests.Selenium
 {
@@ -8,6 +13,16 @@ namespace Trello.Tests.Selenium
         [TestMethod]
         public void TestMethod1()
         {
+            var driverManager = new DriverManager();
+            driverManager.SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+
+            var webDriver = new ChromeDriver();
+
+            webDriver.Navigate().GoToUrl("https://www.google.com");
+
+            Assert.IsTrue(webDriver.Title.Contains("Google"));
+            Thread.Sleep(10000);
+            webDriver.Quit();
         }
     }
 }
