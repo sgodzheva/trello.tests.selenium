f3315a4e4b6fe395c6c874dc65c7eb81040cf524
sgodzheva <svetlozara.godzheva@gmail.com>
2022-05-31 08:25:49 +0300

    Created the initial project

---
+++ src/Trello.Tests.Selenium/Trello.Tests.Selenium/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Trello.Tests.Selenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
